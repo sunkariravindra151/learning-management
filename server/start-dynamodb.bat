a692739a5ae8b979e26eedc0b3478be68aafca47
Ravindra <sunkariravindra03@gmail.com>
2025-06-11 01:15:58 +0530

    one

--- server/start-dynamodb.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-java -Djava.library.path="C:\Users\Sunkari Ravindra\Downloads\dynamodb-local\DynamoDBLocal_lib" ^
- -jar "C:\Users\Sunkari Ravindra\Downloads\dynamodb-local\DynamoDBLocal.jar" ^
- -sharedDb -dbPath "C:\Users\Sunkari Ravindra\Downloads\learning-management"
-pause
